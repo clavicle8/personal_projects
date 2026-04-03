c7a0feac697a8d59dc63022ed8845262dc84d8e0
clavicle8 <calvindai1234@gmail.com>
2026-04-03 21:53:42 +0000

    gaussian generator test function

--- IECsim/gaussian test.cpp
+++ IECsim/gaussian test.cpp
@@ -9,10 +9,11 @@
 #include <cmath>
 using namespace std;
 
+random_device rd{};
+mt19937 gen{rd()};
+normal_distribution<double> d{0.0,1.0};
+
 int main(){
-    random_device rd{};
-    mt19937 gen{rd()};
-    normal_distribution<double> d{0.0,1.0};
     cout << d(gen) << endl;
 }
 
