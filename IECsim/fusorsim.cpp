80f59c2273072f06f87d8cfddfb4cfa95311423d
clavicle8 <calvindai1234@gmail.com>
2026-04-03 21:07:56 +0000

    same as last, wrong button

--- IECsim/fusorsim.cpp
+++ IECsim/fusorsim.cpp
@@ -18,52 +18,60 @@
 #include "particlediagplotter.hpp"
 #include "geomplotter.hpp"
 #include "config.h"
+#include "stl_solid.hpp"
 
 using namespace std;
 
 
 double h = 0.0001; //mesh cell size
-double cathodepot = 5000; //cathode and anode potentials in volts
-double anodepot = 0; 
+double cathodepot = 5000.0; //cathode and anode potentials in volts
+double anodepot = 0.0; 
 
 //tee geom in metres
 double tee_x = 0.127; 
 double tee_y = 0.0955*2;  //measured half
 double tee_z = 0.03175*2; //measured half (radius)
 
+int n_nodes_x = (int) tee_x/h;
+int n_nodes_y = (int) tee_y/h;
+int n_nodes_z = (int) tee_z/h;
+
 void sim(){
-    Geometry geom(MODE_3D, Int3D(tee_x, tee_y, tee_z), Vec3d(0,0,0), h); //define geometry. cuboid with same x,y,z dimensions as tee. 
-    Solid *s1 = chamber("dn63 tee.stl");// import stl for chamber
-    geom.set_solid(s1,7); //pointer
-    STLSolid *s2 cathode("cathodegrid.stl"); //import stl for grid
-    geom.set_solid(s2,8); //pointer
-    STL Solid *s3 anode("anodegrid.stl");
-    geom.set_solid(s3,9)
+    Geometry geom(MODE_3D, Int3D(n_nodes_x, n_nodes_y, n_nodes_z), Vec3D(0,0,0), h); //define geometry. cuboid with same x,y,z dimensions as tee. 
 
-    geom.set_boundary(1, BOUND_NEUMANN, 0);//def bound conditions
-    geom.set_boundary(2, BOUND_NEUMANN, 0);
-    geom.set_boundary(3, BOUND_NEUMANN, 0);
-    geom.set_boundary(4, BOUND_NEUMANN, 0);
-    geom.set_boundary(5, BOUND_NEUMANN, 0);
-    geom.set_boundary(6, BOUND_NEUMANN, 0);
-    geom.set_boundary(7, BOUND_DIRICHLET, 0);
-    geom.set_boundary(8, BOUND_DIRICHLET, cathodepot);
-    geom.set_boundary(9, BOUND_DIRICHLET, anodepot);
+    Solid *s1 = new STLSolid("dn63 tee.stl");
+    geom.set_solid(7,s1);
+    Solid *s2 = new STLSolid("cathodegrid.stl");
+    geom.set_solid(8,s2);
+    Solid *s3 = new STLSolid("anodegrid.stl");
+    geom.set_solid(9,s3);
+    
+    for (uint32_t i = 1; i <= 6; i++){ //loop to set Neumann boundary condition for 6 simulation space faces
+        geom.set_boundary(i, Bound(BOUND_NEUMANN, 0.0));
+    }
 
-    EpotBiCGSTABSolver solver( geom ); //declare biconjugate gradient stabilized method for solver
+    geom.set_boundary(7, Bound(BOUND_DIRICHLET, 0.0));
+    geom.set_boundary(8, Bound(BOUND_DIRICHLET, cathodepot));
+    geom.set_boundary(9, Bound(BOUND_DIRICHLET, anodepot));
 
+    geom.build_mesh();
+    
+    
+    EpotBiCGSTABSolver solver(geom); //declare biconjugate gradient stabilized method for solver
 
     EpotField epot(geom);
     MeshScalarField scharge(geom);
     MeshVectorField bfield;
 
     EpotEfield efield(epot);
+    ParticleDataBase3D pdb(geom);
+    pdb.set_surface_collision(true);
 
     Emittance emit;
     Convergence conv;
     conv.add_epot(epot);
     conv.add_scharge(scharge);
-    conv.add_emittance(emit);
+    conv.add_emittance(0, emit);
 
     //particle declaration loop
     /*
