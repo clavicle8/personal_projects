caa7a58cc68cb29aa67eb9e5b03ee6f09a3af879
clavicle8 <calvindai1234@gmail.com>
2026-04-04 21:21:57 +0000

    added physical parameters to calculate number of particles to be simulated

---
+++ IECsim/physicalparameterstest.cpp
@@ -0,0 +1,23 @@
+#include <cstdlib>
+#include <sstream>
+#include <fstream>
+#include <iomanip>
+#include <random>
+#include <map>
+#include <string>
+#include <cmath>
+
+using namespace std
+
+void main(){
+    //physical parameters
+    double starting_temp = 20.0 + 273.0;
+    double V = 337126.98779 * 1e-9;
+    double R = 8.314; //(joules per mole per kelvin)
+    double k = 1.3806488 * 1e-23; //boltzmann constant
+    double p_mbar = 9 * 1e-3; 
+    double n = p_mbar/(k*starting_temp); //number density
+    double N = n * V
+
+    cout << N << "\n";
+}
