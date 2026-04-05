f9d038afd438f169a0f795efbb329804621a7334
clavicle8 <calvindai1234@gmail.com>
2026-04-05 17:36:15 +0000

    finished setting up the maxwell_dist function

--- IECsim/maxwell_dist.cpp
+++ IECsim/maxwell_dist.cpp
@@ -8,7 +8,12 @@
 #include <string>
 #include <cmath>
 
+ 
+
 using namespace std;
+const double pi = 3.14159265358979323846;
+const double k = 1.3806488 * 1e-23;
+const double R = 8.314;
 
 const double m = 28.0134; // u
 const double q = 1; //e
@@ -17,10 +22,11 @@ const double q = 1; //e
 //Thermal gas molecule speed calculations
 const double T_0 = 20.0 + 273.0;//starting temperature
 const double V = 337126.98779 * 1e-9;
-const double R_const = 8.314; //(joules per mole per kelvin)
-const double k = 1.3806488 * 1e-23; //boltzmann constant
+ //boltzmann constant
 const double a = sqrt(k*T_0/m);
 
 int main(){
+    double mean_velo = 2*a*sqrt(2/pi);
     cout << a << "\n";
+    cout << mean_velo << "\n";
 }
