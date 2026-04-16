61227f636a134bfc2aefda3498200e9a3d47245a
clavicle8 <calvindai1234@gmail.com>
2026-04-16 19:30:53 +0000

    b

--- IECsim/setrun.sh
+++ IECsim/setrun.sh
@@ -3,4 +3,4 @@ RUN=$1
 mkdir -p ${RUN}/plots
 sed -i "s/string run = \"run[0-9]*/string run = \"${RUN}/" fusorsim.cpp
 sed -i "s/run = 'run[0-9]*/run = '${RUN}/" traj_distribution.py
-sed -i "s/run = 'run[0-9]*/run = '${RUN}/" pot_radial.py
+sed -i "s/run = 'run[0-9]*/run = '${RUN}/" pot_radial_allaxes.py
